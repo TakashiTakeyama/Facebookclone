eb5f7c3d72b8da0ebe06348e7974f86dfeec59f9
Takashi <Takashi@gmail.com>
2019-07-14 21:07:02 +0900

    htmlを追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -40,7 +40,6 @@ class PicturesController < ApplicationController
   def create
     # @picture = Picture.new(picture_params)
     @picture = current_user.pictures.build(picture_params)
-
     respond_to do |format|
       if @picture.save
         format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
